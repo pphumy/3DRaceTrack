db55d18109319c209680a637f614d92c0b6712cd
pphumy <keepsilentpo@gmail.com>
2023-05-11 19:05:25 +0700

    move the car

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 position = transform.position;
+        Vector3 angles = transform.eulerAngles;
+
+
+
+        if (position.z > 343)
+        {
+            angles.y = -90;
+            position.x += -speed;
+            transform.eulerAngles = angles;
+            transform.position = position;
+        }
+        else if (position.x > 499)
+        {
+            position.z += speed;
+            transform.position = position;
+        }
+
+        if (position.z < 275)
+        {
+            angles.y = 90;
+            position.x += speed;
+            transform.eulerAngles = angles;
+            transform.position = position;
+        }
+        if (position.x < 499)
+        {
+            angles.y = 180;
+            position.z += -speed;
+            transform.eulerAngles = angles;
+            transform.position = position;
+
+
+        }
+    }
+}
